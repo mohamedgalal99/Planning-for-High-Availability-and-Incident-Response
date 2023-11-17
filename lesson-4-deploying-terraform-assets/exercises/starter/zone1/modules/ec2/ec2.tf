69479e775366a43af070fcbad6fcfc2d6c4de7bc
Mohamed Galal <galal@leaf.cloud>
2023-11-17 11:29:29 +0200

    add required fields

--- lesson-4-deploying-terraform-assets/exercises/starter/zone1/modules/ec2/ec2.tf
+++ lesson-4-deploying-terraform-assets/exercises/starter/zone1/modules/ec2/ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "ubuntu" {
-  ami           = 
-  count = 
-  instance_type = 
+  ami = var.aws_ami
+  count = var.instance_count
+  instance_type = var.instance_type
   subnet_id = var.public_subnet_ids[0]
   vpc_security_group_ids = [aws_security_group.ec2_sg.id]
 
@@ -11,5 +11,38 @@ resource "aws_instance" "ubuntu" {
 }
 
 resource "aws_security_group" "ec2_sg" {
- 
+  name = "ec2_sg"
+  vpc_id = var.vpc_id
+
+  ingress {
+    description = "https port"
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "http port"
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "SSH port"
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    description = "Accept all traffic to outside"
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags {
+    Name = "ec2_sg"
+  }
 }
