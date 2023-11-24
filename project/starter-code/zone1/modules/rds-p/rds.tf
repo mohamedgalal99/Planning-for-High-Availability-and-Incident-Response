4838bb66ab5c36b3a885b4d9651b18136c1ba745
Mohamed Galal <mohamedselim.jobs@gmail.com>
2023-11-24 18:11:51 +0000

    Finished project

--- project/starter-code/zone1/modules/rds-p/rds.tf
+++ project/starter-code/zone1/modules/rds-p/rds.tf
@@ -13,7 +13,7 @@
 
 resource "aws_rds_cluster_parameter_group" "cluster_pg" {
   name   = "udacity-pg-p"
-  family = "aurora5.6"
+  family = "aurora-mysql5.7"
 
   parameter {
     name  = "binlog_format"    
@@ -35,7 +35,8 @@ resource "aws_db_subnet_group" "udacity_db_subnet_group" {
 }
 resource "aws_rds_cluster" "udacity_cluster" {
   cluster_identifier       = "udacity-db-cluster"
-  availability_zones       = ["us-east-2a", "us-east-2b"]
+  availability_zones       = ["us-east-2a", "us-east-2b", "us-east-2c"]
+  engine                   = "aurora-mysql"
   db_cluster_parameter_group_name = aws_rds_cluster_parameter_group.cluster_pg.name
   database_name            = "udacityc2"
   master_username          = "udacity"
@@ -43,9 +44,10 @@ resource "aws_rds_cluster" "udacity_cluster" {
   vpc_security_group_ids   = [aws_security_group.db_sg_1.id]
   db_subnet_group_name     = aws_db_subnet_group.udacity_db_subnet_group.name
   engine_mode              = "provisioned"
-  engine_version           = "5.6.mysql_aurora.1.19.1" 
+  engine_version           = "5.7.mysql_aurora.2.11.4" 
   skip_final_snapshot      = true
   storage_encrypted        = false
+  backup_retention_period  = 5
   depends_on = [aws_rds_cluster_parameter_group.cluster_pg]
 }
 
@@ -54,13 +56,14 @@ output "db_cluster_arn" {
 }
 
 output "db_instance_arn" {
-  value = aws_rds_cluster_instance.udacity_instance[0].arn
+  value = aws_rds_cluster_instance.udacity_instance[1].arn
 }
 
 resource "aws_rds_cluster_instance" "udacity_instance" {
-  count                = 1
+  count                = 2
   identifier           = "udacity-db-instance-${count.index}"
   cluster_identifier   = aws_rds_cluster.udacity_cluster.id
+  engine               = aws_rds_cluster.udacity_cluster.engine
   instance_class       = "db.t2.small"
   db_subnet_group_name = aws_db_subnet_group.udacity_db_subnet_group.name
 }
